d2a9799bcda99d784b12d09bb9549ecf0442d285
Eduardo Poleo <eduardopoleo@gmail.com>
2015-06-14 12:06:50 -0400

    First commit

--- server.rb
+++ server.rb
@@ -18,23 +18,23 @@ puts "Server started: http://localhost:#{port}/"
 root = File.expand_path './public'
 server = WEBrick::HTTPServer.new :Port => port, :DocumentRoot => root
 
-server.mount_proc '/comments.json' do |req, res|
-  comments = JSON.parse(File.read('./comments.json'))
+server.mount_proc '/todos.json' do |req, res|
+  todos = JSON.parse(File.read('./todos.json'))
 
   if req.request_method == 'POST'
     # Assume it's well formed
-    comment = {}
+    todo = {}
     req.query.each do |key, value|
-      comment[key] = value.force_encoding('UTF-8')
+      todo[key] = value.force_encoding('UTF-8')
     end
-    comments << comment
-    File.write('./comments.json', JSON.pretty_generate(comments, :indent => '    '))
+    todos << todo
+    File.write('./todos.json', JSON.pretty_generate(todos, :indent => '    '))
   end
 
   # always return json
   res['Content-Type'] = 'application/json'
   res['Cache-Control'] = 'no-cache'
-  res.body = JSON.generate(comments)
+  res.body = JSON.generate(todos)
 end
 
 trap 'INT' do server.shutdown end
